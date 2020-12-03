69ed771596399ebbd2bc20834569f8d65a3589d4
LeRoy Dahl <ldahl2018@gmail.com>
2020-12-03 16:52:20 -0600

    get server route for getting pets in db

--- app.py
+++ app.py
@@ -1,24 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 import psycopg2
 
-conn = psycopg2.connect(
-    dbname="pet_hotel",
-)
+conn = psycopg2.connect(dbname="pet_hotel",)
 
 cur = conn.cursor()
 
-cur.execute('SELECT * from owners;')
-
-records = cur.fetchall()
-
-print(records)
-    # user="leroydahl",
-    # password="Randy2020!"
-
 app = Flask(__name__)
 
-@app.route('/')
-def helloWorld():
-    return {'hello': 'world'}
+@app.route('/pets')
+def getPets():
+    cur.execute('SELECT * from pets;')
+    records = cur.fetchall()
+    return jsonify(records[1])
+
 
 app.run()
