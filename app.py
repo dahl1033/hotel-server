60e83d12f77846a33205783b861babcb83b90e23
LeRoy Dahl <ldahl2018@gmail.com>
2020-12-03 13:45:26 -0600

    connected server to DB

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 from flask import Flask
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname="pet_hotel",
+)
+
+cur = conn.cursor()
+
+cur.execute('SELECT * from owners;')
+
+records = cur.fetchall()
+
+print(records)
+    # user="leroydahl",
+    # password="Randy2020!"
 
 app = Flask(__name__)
 
